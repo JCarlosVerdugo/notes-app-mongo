4415d9632bd2f742b75d596de8c51fc45862d161
Carlos Verdugo <carlosverdugo@proton.me>
2024-09-22 19:57:36 -0700

    Adding handlebars

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join( app.get('views'), 'layouts'),
+    partialsDir: path.join( app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended: false}));
@@ -16,7 +24,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('index');
 });
 
 // Static files
