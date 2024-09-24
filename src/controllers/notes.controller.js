65c5406c582be1a86cef2a111a107ce7402220e1
Carlos Verdugo <carlosverdugo@proton.me>
2024-09-24 15:52:37 -0700

    Adding delete and edit

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,16 +1,24 @@
 const notesCtrl = {};
 
+const Note = require('./../models/Note');
+
 notesCtrl.renderNoteForm = (req, res) => {
     res.render('notes/new-note');
 }
 
-notesCtrl.createNewNote = (req, res) => {
+notesCtrl.createNewNote = async (req, res) => {
     const {title, description} = req.body;
-    res.send('new note');
+    const newNote = new Note({
+        title,
+        description
+    });
+    await newNote.save();
+    res.redirect('/notes');
 }
 
-notesCtrl.renderNotes = (req, res) => {
-    res.send('render notes');
+notesCtrl.renderNotes = async (req, res) => {
+    const notes = await Note.find().lean();
+    res.render('notes/all-notes', { notes });
 }
 
 notesCtrl.renderEditForm = (req, res) => {
@@ -21,8 +29,9 @@ notesCtrl.updateNote = (req, res) => {
     res.send('update notes')
 }
 
-notesCtrl.deleteNote = (req, res) => {
-    res.send('delete note')
+notesCtrl.deleteNote = async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id)
+    res.redirect('/notes');
 }
 
 module.exports = notesCtrl;
